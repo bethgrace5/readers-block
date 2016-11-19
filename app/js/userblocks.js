74d82914b859177adce6b78f259d6aa524ec8276
Cory Kitchens <corykitchens@gmail.com>
2016-11-19 14:38:30 -0800

    Basic functionality of creating blocks

--- app/js/userblocks.js
+++ app/js/userblocks.js
@@ -1,9 +1,16 @@
 'use strict';
 angular.module('readers-block')
   .controller('UserBlocksCtrl', function ($scope, $http, loginFactory){
+    $scope.userBlocks = [];
     $scope.newBlock = {
       title: "",
       description: "",
       tags: ""
     };
+
+    $scope.createNewBlock = function() {
+      $scope.userBlocks.push($scope.newBlock);
+      $scope.newBlock = {};
+    }
+
 });
