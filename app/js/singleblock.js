4e7c3d41e460e74414439a723d1670c310b3e6da
Bethany Armitage <bethgrace5@gmail.com>
2016-11-20 17:50:11 -0800

    rating default

--- app/js/singleblock.js
+++ app/js/singleblock.js
@@ -52,6 +52,10 @@ angular.module('readers-block')
       newBook.title = book.title;
       newBook.author = book.author;
       newBook.thumbnail_image = book.thumbnail_image;
+      newBook.rating: {
+       community_rating: 2.5,
+       user_rating: 3 
+      },
       updateBookList(newBook);
     };
 
